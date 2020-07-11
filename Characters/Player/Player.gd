555ceb6f2fe14232d07effdcee013e8c8a3e5bf1
Timmy <btyang24@gmail.com>
2020-07-10 22:36:18 -0700

    Restructured player scene tree

--- Characters/Player/Player.gd
+++ Characters/Player/Player.gd
@@ -1,20 +1,28 @@
 extends KinematicBody2D
 
+export(NodePath) var tilemap_reference
+
 var location = Vector2()
 var speed = 128
 
+onready var map = get_node(tilemap_reference)
+
+func _ready():
+	position = map.map_to_world(Vector2(0,0)) + Vector2(128, 128)
+	print('moved to position...')
+
 func _physics_process(delta):
 	pass
-#	if Input.is_action_just_pressed("ui_up"):
-#		location.y = location.y-speed
-#	elif Input.is_action_just_pressed("ui_down"):
-#		location.y = location.y+speed
-#	elif Input.is_action_just_pressed("ui_right"):
-#		location.x = location.x+speed
-#	elif Input.is_action_just_pressed("ui_left"):
-#		location.x = location.x-speed
-##
-#	set_position(location)
+	if Input.is_action_just_pressed("ui_up"):
+		location.y = location.y-speed
+	elif Input.is_action_just_pressed("ui_down"):
+		location.y = location.y+speed
+	elif Input.is_action_just_pressed("ui_right"):
+		location.x = location.x+speed
+	elif Input.is_action_just_pressed("ui_left"):
+		location.x = location.x-speed
+#
+	set_position(location)
 
 func move_up():
 	location.y = location.y-speed
