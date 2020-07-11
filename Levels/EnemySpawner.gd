27951d02f2029e38d0d49ecef5da28fb06c46783
KokoDoko <mykosocial31@gmail.com>
2020-07-11 16:30:31 -0700

    Updated Spawner RNG and Speed

--- Levels/EnemySpawner.gd
+++ Levels/EnemySpawner.gd
@@ -35,265 +35,266 @@ func _on_respawnTimer_timeout():
 		e.position = location
 		e.move_left()
 	
+	e.speed = randi()%600 + 100
+	
 	$respawnTimer.wait_time = max(.01, $respawnTimer.wait_time - .001)
 	
 	
 	
 func randomTopSpawn(playerLoc):
-	var rngCell = randi()%15 +1
+	var rngCell = randi()%27 +1
 	var location = Vector2()
 	
 	
-	if rngCell == 1:
+	if rngCell >= 1 and rngCell <= 5:
 		location.x = playerLoc.x+0
 		location.y = playerLoc.y-896
 	
-	elif rngCell == 2:
+	elif rngCell >= 6 and rngCell <= 10:
 		location.x = playerLoc.x+128
 		location.y = playerLoc.y-896
 	
-	elif rngCell == 3:
-		location.x = playerLoc.x+256
-		location.y = playerLoc.y-896
-	
-	elif rngCell == 4:
-		location.x = playerLoc.x+384
-		location.y = playerLoc.y-896
-	
-	elif rngCell == 5:
-		location.x = playerLoc.x+512
-		location.y = playerLoc.y-896
-	
-	elif rngCell == 6:
-		location.x = playerLoc.x+640
-		location.y = playerLoc.y-896
-
-	elif rngCell == 7:
-		location.x = playerLoc.x+768
-		location.y = playerLoc.y-896
-		
-	elif rngCell == 8:
-		location.x = playerLoc.x+896
-		location.y = playerLoc.y-896
-		
-	elif rngCell == 9:
+	elif rngCell >= 11 and rngCell <= 15:
 		location.x = playerLoc.x-128
 		location.y = playerLoc.y-896
 	
-	elif rngCell == 10:
+	elif rngCell == 16:
+		location.x = playerLoc.x+384
+		location.y = playerLoc.y-896
+	
+	elif rngCell == 17:
+		location.x = playerLoc.x+512
+		location.y = playerLoc.y-896
+	
+	elif rngCell == 18:
+		location.x = playerLoc.x+640
+		location.y = playerLoc.y-896
+
+	elif rngCell == 19:
+		location.x = playerLoc.x+768
+		location.y = playerLoc.y-896
+		
+	elif rngCell == 20:
+		location.x = playerLoc.x+896
+		location.y = playerLoc.y-896
+		
+	elif rngCell == 21:
+		location.x = playerLoc.x+256
+		location.y = playerLoc.y-896
+	
+	elif rngCell == 22:
 		location.x = playerLoc.x-256
 		location.y = playerLoc.y-896
 	
-	elif rngCell == 11:
+	elif rngCell == 23:
 		location.x = playerLoc.x-384
 		location.y = playerLoc.y-896
 	
-	elif rngCell == 12:
+	elif rngCell == 24:
 		location.x = playerLoc.x-512
 		location.y = playerLoc.y-896
 	
-	elif rngCell == 13:
+	elif rngCell == 25:
 		location.x = playerLoc.x-640
 		location.y = playerLoc.y-896
 
-	elif rngCell == 14:
+	elif rngCell == 26:
 		location.x = playerLoc.x-768
 		location.y = playerLoc.y-896
 		
-	elif rngCell == 15:
+	elif rngCell == 27:
 		location.x = playerLoc.x-896
 		location.y = playerLoc.y-896
 	
 	return(location)
 	
 func randomBottomSpawn(playerLoc):
-	var rngCell = randi()%15 +1
+	var rngCell = randi()%27 +1
 	var location = Vector2()
 	
 	
-	if rngCell == 1:
+	if rngCell >= 1 and rngCell <= 5:
 		location.x = playerLoc.x+0
 		location.y = playerLoc.y+896
 	
-	elif rngCell == 2:
+	elif rngCell >= 6 and rngCell <= 10:
 		location.x = playerLoc.x+128
 		location.y = playerLoc.y+896
 	
-	elif rngCell == 3:
-		location.x = playerLoc.x+256
-		location.y = playerLoc.y+896
-	
-	elif rngCell == 4:
-		location.x = playerLoc.x+384
-		location.y = playerLoc.y+896
-	
-	elif rngCell == 5:
-		location.x = playerLoc.x+512
-		location.y = playerLoc.y+896
-	
-	elif rngCell == 6:
-		location.x = playerLoc.x+640
-		location.y = playerLoc.y+896
-
-	elif rngCell == 7:
-		location.x = playerLoc.x+768
-		location.y = playerLoc.y+896
-		
-	elif rngCell == 8:
-		location.x = playerLoc.x+896
-		location.y = playerLoc.y+896
-		
-	elif rngCell == 9:
+	elif rngCell >= 11 and rngCell <= 15:
 		location.x = playerLoc.x-128
 		location.y = playerLoc.y+896
 	
-	elif rngCell == 10:
+	elif rngCell == 16:
+		location.x = playerLoc.x+384
+		location.y = playerLoc.y+896
+	
+	elif rngCell == 17:
+		location.x = playerLoc.x+512
+		location.y = playerLoc.y+896
+	
+	elif rngCell == 18:
+		location.x = playerLoc.x+640
+		location.y = playerLoc.y+896
+
+	elif rngCell == 19:
+		location.x = playerLoc.x+768
+		location.y = playerLoc.y+896
+		
+	elif rngCell == 20:
+		location.x = playerLoc.x+896
+		location.y = playerLoc.y+896
+		
+	elif rngCell == 21:
+		location.x = playerLoc.x+256
+		location.y = playerLoc.y+896
+	
+	elif rngCell == 22:
 		location.x = playerLoc.x-256
 		location.y = playerLoc.y+896
 	
-	elif rngCell == 11:
+	elif rngCell == 23:
 		location.x = playerLoc.x-384
 		location.y = playerLoc.y+896
 	
-	elif rngCell == 12:
+	elif rngCell == 24:
 		location.x = playerLoc.x-512
 		location.y = playerLoc.y+896
 	
-	elif rngCell == 13:
+	elif rngCell == 25:
 		location.x = playerLoc.x-640
 		location.y = playerLoc.y+896
 
-	elif rngCell == 14:
+	elif rngCell == 26:
 		location.x = playerLoc.x-768
 		location.y = playerLoc.y+896
 		
-	elif rngCell == 15:
+	elif rngCell == 27:
 		location.x = playerLoc.x-896
 		location.y = playerLoc.y+896
 	
 	return(location)
 	
 func randomLeftSpawn(playerLoc):
-	var rngCell = randi()%14 +1
+	var rngCell = randi()%26 +1
 	var location = Vector2()
 	
-	if rngCell == 1:
+	if rngCell >= 1 and rngCell <= 5:
 		location.y = playerLoc.y+0
 		location.x = playerLoc.x-1216
 	
-	elif rngCell == 2:
+	elif rngCell >= 6 and rngCell <= 10:
 		location.y = playerLoc.y+128
 		location.x = playerLoc.x-1216
 	
-	elif rngCell == 3:
+	elif rngCell == 16:
 		location.y = playerLoc.y+256
 		location.x = playerLoc.x-1216
 	
-	elif rngCell == 4:
+	elif rngCell == 17:
 		location.y = playerLoc.y+384
 		location.x = playerLoc.x-1216
 	
-	elif rngCell == 5:
+	elif rngCell == 18:
 		location.y = playerLoc.y+512
 		location.x = playerLoc.x-1216
 	
-	elif rngCell == 6:
+	elif rngCell == 19:
 		location.y = playerLoc.y+640
 		location.x = playerLoc.x-1216
 
-	elif rngCell == 7:
+	elif rngCell == 20:
 		location.y = playerLoc.y+768
 		location.x = playerLoc.x-1216
 		
-	elif rngCell == 8:
+	elif rngCell >= 10 and rngCell <= 15:
 		location.y = playerLoc.y-128
 		location.x = playerLoc.x-1216
 	
-	elif rngCell == 9:
+	elif rngCell == 21:
 		location.y = playerLoc.y-256
 		location.x = playerLoc.x-1216
 	
-	elif rngCell == 10:
+	elif rngCell == 22:
 		location.y = playerLoc.y-384
 		location.x = playerLoc.x-1216
 	
-	elif rngCell == 11:
+	elif rngCell == 23:
 		location.y = playerLoc.y-512
 		location.x = playerLoc.x-1216
 	
-	elif rngCell == 12:
+	elif rngCell == 24:
 		location.y = playerLoc.y-640
 		location.x = playerLoc.x-1216
 	
-	elif rngCell == 13:
+	elif rngCell == 25:
 		location.y = playerLoc.y-768
 		location.x = playerLoc.x-1216
 
-	elif rngCell == 14:
+	elif rngCell == 26:
 		location.y = playerLoc.y-896
 		location.x = playerLoc.x-1216
 
 	return(location)
 	
 func randomRightSpawn(playerLoc):
-	var rngCell = randi()%14 +1
+	var rngCell = randi()%26 +1
 	var location = Vector2()
 	
-	if rngCell == 1:
-		location.y = playerLoc.y+64
+	if rngCell >= 1 and rngCell <= 5:
+		location.y = playerLoc.y+0
 		location.x = playerLoc.x+1216
 	
-	elif rngCell == 2:
-		location.y = playerLoc.y+192
+	elif rngCell >= 6 and rngCell <= 10:
+		location.y = playerLoc.y+128
 		location.x = playerLoc.x+1216
 	
-	elif rngCell == 3:
-		location.y = playerLoc.y+320
+	elif rngCell == 16:
+		location.y = playerLoc.y+256
 		location.x = playerLoc.x+1216
 	
-	elif rngCell == 4:
-		location.y = playerLoc.y+448
+	elif rngCell == 17:
+		location.y = playerLoc.y+384
 		location.x = playerLoc.x+1216
 	
-	elif rngCell == 5:
-		location.y = playerLoc.y+576
+	elif rngCell == 18:
+		location.y = playerLoc.y+512
 		location.x = playerLoc.x+1216
 	
-	elif rngCell == 6:
-		location.y = playerLoc.y+704
+	elif rngCell == 19:
+		location.y = playerLoc.y+640
 		location.x = playerLoc.x+1216
 
-	elif rngCell == 7:
-		location.y = playerLoc.y+832
+	elif rngCell == 20:
+		location.y = playerLoc.y+768
 		location.x = playerLoc.x+1216
 		
-	elif rngCell == 8:
-		location.y = playerLoc.y-64
+	elif rngCell >= 10 and rngCell <= 15:
+		location.y = playerLoc.y-128
 		location.x = playerLoc.x+1216
 	
-	elif rngCell == 9:
-		location.y = playerLoc.y-192
+	elif rngCell == 21:
+		location.y = playerLoc.y-256
 		location.x = playerLoc.x+1216
 	
-	elif rngCell == 10:
-		location.y = playerLoc.y-320
+	elif rngCell == 22:
+		location.y = playerLoc.y-384
 		location.x = playerLoc.x+1216
 	
-	elif rngCell == 11:
-		location.y = playerLoc.y-448
+	elif rngCell == 23:
+		location.y = playerLoc.y-512
 		location.x = playerLoc.x+1216
 	
-	elif rngCell == 12:
-		location.y = playerLoc.y-576
+	elif rngCell == 24:
+		location.y = playerLoc.y-640
 		location.x = playerLoc.x+1216
 	
-	elif rngCell == 13:
-		location.y = playerLoc.y-704
+	elif rngCell == 25:
+		location.y = playerLoc.y-768
 		location.x = playerLoc.x+1216
 
-	elif rngCell == 14:
-		location.y = playerLoc.y-832
+	elif rngCell == 26:
+		location.y = playerLoc.y-896
 		location.x = playerLoc.x+1216
 
-	location.y = location.y+64
 	return(location)
