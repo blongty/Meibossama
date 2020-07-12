27eb581d8303d890c86353159849390bb977965a
Timmy <btyang24@gmail.com>
2020-07-12 10:44:42 -0700

    Help menu implementation

--- UI/Title/title.gd
+++ UI/Title/title.gd
@@ -6,9 +6,13 @@ var highscore
 export(NodePath) var trans_ref
 var trans
 
+export(NodePath) var help_ref
+var help
+
 func _ready():
 	highscore = get_node(highscore_ref)
 	trans = get_node(trans_ref)
+	help = get_node(help_ref)
 	
 	# Get latest high score
 	var file = File.new()
@@ -29,3 +33,11 @@ func _on_Start_pressed():
 	yield(trans, 'anim_done')
 	get_tree().change_scene("res://Levels/main.tscn")
 
+
+
+func _on_Help_pressed():
+	help.visible = true
+
+
+func _on_Return_pressed():
+	help.visible = false
